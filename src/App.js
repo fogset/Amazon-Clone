33a90b82b8115fa1956049f02c299cd9c990321b
fogset <zhangth1990@hotmail.com>
2021-05-31 23:47:07 -0700

    setting up payment method layout

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
 import Payment from './Payment';
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js"
+
+const promise = loadStripe(
+  "pk_test_51IuoBaIC9oqOH2EBYrWzYVxTp2EVRzy4WvtNrYcmnQoCSrmlnymxKMP7MvYLJbdBAT0uuMff57tvoKswnu8qrjag000KlZTWp1"
+)
 
 function App() {
   const [{ }, dispatch] = useStateValue();
@@ -50,7 +56,9 @@ function App() {
 
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
 
           <Route path="/">
--- src/Payment.js
+++ src/Payment.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Payment.css";
 import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
 import { Link } from "react-router-dom"
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from './reducer';
 
 function Payment() {
     const [{ basket, user }, dispatch] = useStateValue();
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
+
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+
+
+    const handleSubmit = e => {
+        // do all the facy strip
+    }
+
+    const handleChange = e => {
+        // setDisabled(event.empty);
+        // setError(event.error ? event.error.message : "");
+
+    }
+
+
     return (
         <div className='payment'>
             <div className='payment__container'>
@@ -49,12 +74,31 @@ function Payment() {
                     </div>
                     <div className="payment__details">
                         {/* {stripe magic} */}
+
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange} />
+                            <div className='payment__priceContainer'>
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                        <h3>Order Total: {value}</h3>
+                                    )}
+                                    decimalScale={2}
+                                    value={getBasketTotal(basket)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"$"}
+                                />
+                                <button disabled={processing || disabled || succeeded}></button>
+                                <span>{processing ?
+                                    <p>Processing</p> : "Buy Now"}</span>
+                            </div>
+                        </form>
                     </div>
                 </div>
 
             </div>
 
-        </div>
+        </div >
     )
 }
 
