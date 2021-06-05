0fb00325c79ac959ed9fcac19d7854f601a74d41
fogset <zhangth1990@hotmail.com>
2021-06-05 16:16:12 -0700

    show product from firebase

--- src/Home.js
+++ src/Home.js
@@ -10,25 +10,27 @@ function Home() {
 
     function getProducts() {
         setLoading(true);
-        db.collection("products").onSnapshot((querySnapshot) => {
-            const items = [];
-            querySnapshot.forEach((doc) => {
-                items.push(doc.data());
-            });
-            setProducts(items);
-            setLoading(false);
-        });
+
+        db.collection("products").get()
+            .then(snapshot => {
+                const items = [];
+                snapshot.forEach(doc => {
+                    items.push(doc.data());
+                })
+                setProducts(items);
+
+            })
+            .catch(error => console.log(error))
+
     }
 
     useEffect(() => {
         getProducts();
-        console.log("load from firestore ----------------");
-        console.log(products);
+        // console.log("load from firestore ----------------");
+        // console.log(products);
     }, []);
 
-    if (loading) {
-        return <h1>Loading...</h1>
-    }
+
 
     return (
         <div className="home">
@@ -37,53 +39,30 @@ function Home() {
                     className="home__image"
                     src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
                 />
+                <div className="home__row">
+                    {products.map(product => (
+                        <Product
+                            title={product.ProductName}
+                            price={product.ProductPrice}
+                            image={product.ProductImg}
+                            rating={3}
+                        />
+                    ))}
+
+                </div>
 
                 <div className="home__row">
                     <Product
-                        title={products ? products[0].ProductName : null}
-                        price={products ? products[0].ProductPrice : null}
-                        image={products ? products[0].ProductImg : null}
+                        title={products[0] && products[0].ProductName}
+                        price={products[0] && products[0].ProductPrice}
+                        image={products[0] && products[0].ProductImg}
                         rating={3}
                     />
-                    <Product
-                        title="very nice keyboard"
-                        price={300.99}
-                        image="https://picsum.photos/200/300"
-                        rating={4}
-                    />
                 </div>
 
-                <div className="home__row">
-                    <Product
-                        title="ASUS ROG Strix G15 15.6 Gaming Laptop"
-                        price={999.99}
-                        image="https://multimedia.bbycastatic.ca/multimedia/products/500x500/153/15345/15345387_1.jpg"
-                        rating={4}
-                    />
-                    <Product
-                        title="ASUS TUF 15.6 Gaming Laptop"
-                        price={849.99}
-                        image="https://multimedia.bbycastatic.ca/multimedia/products/1500x1500/146/14679/14679322.jpg"
-                        rating={4}
-                    />
-                    <Product
-                        title="Dell G3 15.6 Gaming Laptop"
-                        price={81399.99}
-                        image="https://multimedia.bbycastatic.ca/multimedia/products/500x500/146/14682/14682947.jpg"
-                        rating={4}
-                    />
-                </div>
-                <div className="home__row">
-                    <Product
-                        title="Dell G3 15.6 Gaming Laptop"
-                        price={8399.99}
-                        image="https://multimedia.bbycastatic.ca/multimedia/products/500x500/146/14682/14682947.jpg"
-                        rating={5}
-                    />
-                </div>
 
             </div>
-        </div>
+        </div >
     )
 
 }
